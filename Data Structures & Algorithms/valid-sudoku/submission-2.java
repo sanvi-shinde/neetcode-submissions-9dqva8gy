2eb58ca2412fe255241068aeede02184320cf5d2
sanvi-shinde <pallavi.naikwade27@gmail.com>
2026-04-01 13:30:36 -0400

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> set = new HashSet<>();
+        for(int i  = 0 ; i < 9 ; i++){
+            for(int j = 0 ; j < 9 ; j++){
+                if(board[i][j] == '.'){
+                     continue;
+                }
+                
+                String val = String.valueOf(board[i][j]);
+                String row = val + "ROW" + i;
+                String col = val + "COL" + j;
+                String box = val + "BOX" + i / 3 + "_" + j/3;
+                if(!set.add(row) || !set.add(col) || !set.add(box)){
+                    return false;
+                }
+            }
+        }
+        
+       return true; 
+    }
+}
